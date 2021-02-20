d4da099c7c6eaaf625f170383718cd716cee68a2
Mark Gillespie <mark.gillespie81@gmail.com>
2021-02-20 12:19:21 -0500

    Record words

--- toggle.js
+++ toggle.js
@@ -7,3 +7,28 @@ while (board_letters.length < 100) {
     .replace(/[^a-z]+/g, "");
 }
 drawBoard(10, 10, board_letters);
+
+// TODO
+function checkWordOkay(word) {
+  return word.length > 4;
+}
+
+function addToWordList(word, isOkay) {
+  const el = document.createElement("span");
+  el.innerHTML = word;
+  el.classList.add(isOkay ? "good" : "bad");
+  const wordList = document.getElementById("found-words");
+  if (wordList.firstChild) {
+    wordList.insertBefore(el, wordList.firstChild);
+  } else {
+    wordList.appendChild(el);
+  }
+}
+
+document.getElementById("guess-word").addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    const word = e.target.value;
+    addToWordList(word, checkWordOkay(word));
+    e.target.value = "";
+  }
+});
